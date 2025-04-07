f60b54e0742d29e5a2f2fc93c6e5f710c7e06f23
Sina Jahromi <sina.jhrm@gmail.com>
2025-04-06 20:16:12 -0400

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,12 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt => opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
