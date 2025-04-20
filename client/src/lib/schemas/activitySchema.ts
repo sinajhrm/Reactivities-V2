3ef69f65738ff6daac4644fbe5ae1cb189d8a7d6
Sina Jahromi <sina.jhrm@gmail.com>
2025-04-20 11:28:10 -0400

    End of section 11

---
+++ client/src/lib/schemas/activitySchema.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+const requiredString = (fieldName: string) =>
+  z
+    .string({ required_error: `${fieldName} is required!` })
+    .min(1, { message: `${fieldName} is required!` });
+
+export const activitySchema = z.object({
+  title: requiredString("Title"),
+  description: requiredString("Description"),
+  category: requiredString("Category"),
+  date: z.coerce
+    .date({ message: "Date is required!" })
+    .min(new Date(), "Date must be in the future!"),
+  location: z.object({
+    venue: requiredString("Venue"),
+    city: z.string().optional(),
+    latitude: z.coerce
+      .number()
+      .min(-90, "Latitude must be greater than -90")
+      .max(90, "Latitude must be less than 90"),
+    longitude: z.coerce
+      .number()
+      .min(-180, "Longitude must be greater than -90")
+      .max(180, "Longitude must be less than 90"),
+  }),
+});
+
+export type ActivitySchema = z.infer<typeof activitySchema>;
